5fbb2788fcf1c8207f85347cde780eaa7241f275
chocovish <vishal.ghosh28@gmail.com>
2019-02-17 14:15:21 +0530

    Endpoints added

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .serializers import UserCreateSerializer,InviteMemberSerializer,MemberDetailSerializer,FamilyDetailSerializer
 from .models import User
-from rest_framework.generics import CreateAPIView,ListAPIView,RetrieveAPIView
+from rest_framework.generics import CreateAPIView,ListAPIView,RetrieveUpdateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 #from django.db.models.Model import DoesNotExist
 
-class UserRetriveView(RetrieveAPIView):
+class UserRetriveView(RetrieveUpdateAPIView):
     queryset = User.objects.all()
     lookup_field = 'phone'
     serializer_class = MemberDetailSerializer
